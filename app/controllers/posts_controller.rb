f6fdb9f9c4745b193cd387da19c832e41c49abfc
john03191 <john03tgck01a@gmail.com>
2020-06-23 12:44:05 +0900

    投稿機能の実装完了
    投稿したユーザーしか編集できない
    その他のユーザーは戻るボタンのみ表示

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,6 @@
 class PostsController < ApplicationController
   before_action :set_post, only: [:show, :edit, :update]
-  before_action :move_to_index, except: [:index, :show]
+  before_action :move_to_index, except: :index
 
   def index
     @posts = Post.all.order(id: "DESC")
